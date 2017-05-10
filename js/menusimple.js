6e603f9a124272297279cc9294895ea950cafec5
Nazarii Petrusiv <naz@petrusiv.pro>
2017-05-10 13:48:20 +0200

    first commit

---
+++ js/menusimple.js
@@ -0,0 +1,9 @@
+/* Set the width of the side navigation to 370px */
+function openNav() {
+    document.getElementById("chSidenav").style.width = "75%";
+}
+
+/* Set the width of the side navigation to 0 */
+function closeNav() {
+    document.getElementById("chSidenav").style.width = "0";
+}
